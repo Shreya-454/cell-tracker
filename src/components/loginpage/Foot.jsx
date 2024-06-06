233e12d1a1793e3dbe0f464baaeef001e0244a97
Priyanka-390 <psr82220@gmail.com>
2024-06-06 14:52:48 +0530

    testing

--- src/components/loginpage/Foot.jsx
+++ src/components/loginpage/Foot.jsx
@@ -3,16 +3,32 @@ import LinKDin from "../../assets/svg/linkDin.svg";
 import Insta from "../../assets/svg/insta.svg";
 import FaceBook from "../../assets/svg/faceBook.svg";
 import Twitter from "../../assets/svg/twitter.svg";
+import leftelipse from "../../assets/images/svg/leftAdvantage.svg"
+import rightelipse from "../../assets/images/svg/rightAdvantage.svg"
 
 const Foot = () => {
     const  date= new Date();
     const year = date.getFullYear();
   return (
     <div className="pt-[102px] max-md:pt-[58px] z-[1] relative">
+      <img
+        src={leftelipse}
+        alt="ellipse"
+        className="absolute top-[-14%] md:block hidden left-0  z-[-1] max-sm:w-[41px] max-lg:max-w-[64px]"
+      />
+      <img
+        src={rightelipse}
+        alt="ellipse"
+        className="absolute top-[-26%] right-0 md:hidden flex z-[-1] max-sm:w-[41px] max-lg:max-w-[64px]"
+      />
       <div className="container max-w-[1140px] px-3">
         <div className="flex flex-row items-center pb-[46px] flex-wrap -mx-3">
           <div className="md:w-1/3 w-full md:justify-start flex justify-center">
-            <img src={footerlogo} alt="footerlogo" className=" max-sm:h-[29px] max-sm:w-[211px]" />
+            <img
+              src={footerlogo}
+              alt="footerlogo"
+              className=" max-sm:h-[29px] max-sm:w-[211px]"
+            />
           </div>
           <div className="md:w-1/3 max-md:my-8 max-md:pt-[6px] w-full">
             <ul className="flex gap-6 justify-center">
@@ -21,23 +37,23 @@ const Foot = () => {
                   href=""
                   className="sm:text-base footerlinkhover text-sm text-lightBlack font-normal"
                 >
-                Home
+                  Home
                 </a>
               </li>
               <li>
                 <a
-                  href=""
+                  href="#work"
                   className="sm:text-base footerlinkhover text-sm text-lightBlack font-normal"
                 >
-   How It Works
+                  How It Works
                 </a>
               </li>
               <li>
                 <a
-                  href=""
+                  href="#features"
                   className="sm:text-base footerlinkhover text-sm text-lightBlack font-normal"
                 >
-              Features
+                  Features
                 </a>
               </li>
             </ul>
@@ -45,7 +61,7 @@ const Foot = () => {
           <div className="md:w-1/3 w-full gap-[18px] flex md:justify-end justify-center">
             <a
               href=""
-              className="md:w-[40px] group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
+              className="md:w-[40px] hover:shadow-common-shadow2 duration-300 group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
             >
               <img
                 src={LinKDin}
@@ -55,7 +71,7 @@ const Foot = () => {
             </a>
             <a
               href=""
-              className="md:w-[40px] group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
+              className="md:w-[40px] hover:shadow-common-shadow2 duration-300 group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
             >
               <img
                 src={Insta}
@@ -65,7 +81,7 @@ const Foot = () => {
             </a>
             <a
               href=""
-              className="md:w-[40px] group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
+              className="md:w-[40px] hover:shadow-common-shadow2 duration-300 group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
             >
               <img
                 src={FaceBook}
@@ -75,7 +91,7 @@ const Foot = () => {
             </a>
             <a
               href=""
-              className="md:w-[40px] group md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
+              className="md:w-[40px] group hover:shadow-common-shadow2 duration-300 md:h-[40px] w-[28px] h-[28px] rounded-full border border-darkBlue flex justify-center items-center"
             >
               <img
                 src={Twitter}
--- src/components/loginpage/Nav.jsx
+++ src/components/loginpage/Nav.jsx
@@ -14,8 +14,10 @@ const Nav = () => {
   return (
     <nav>
       <div className="container max-w-[1188px] pb-[10px] pt-8 md:pt-[27px] md:pb-[21px] flex items-center justify-between">
-      <a className="relative z-50 " href="http://">
-          <NavLogo className={` !fill-white ${navShow ? "max-md:!fill-darkBlue" : ""}`} />
+        <a className="relative z-50 " href="http://">
+          <NavLogo
+            className={` !fill-white ${navShow ? "max-md:!fill-darkBlue" : ""}`}
+          />
         </a>
         <div
           className={`flex items-center ${
@@ -23,35 +25,54 @@ const Nav = () => {
           } navbar-toggle duration-300 max-md:pt-[120px] text-nowrap md:w-[60%] md:justify-between`}
         >
           <ul className="flex items-center gap-5 md:gap-[35px] max-md:flex-col">
-        <li>
-        <NavLink to="/"
+            <li>
+              <NavLink
+                to="/"
+                rel="noopener noreferrer"
                 onClick={() => setnavShow(!navShow)}
-                className={`leading-[26.59px] text-black text-base font-normal navlinkhover md:text-white flex items-center gap-[6px] ${({ isActive, isPending }) =>
-                isActive ? "after:absolute relative hover:after:left-[0] after:bottom-0 after:w-0 after:left-[50%] after:rounded-[10px] after:h-[2px] after:bg-white hover:after:w-full after:duration-300" : ""
-                  }`}
+                className={`leading-[26.59px] text-black text-base font-normal navlinkhover md:text-white flex items-center gap-[6px] ${({
+                  isActive,
+                  isPending,
+                }) =>
+                  isActive
+                    ? "after:absolute relative hover:after:left-[0] after:bottom-0 after:w-0 after:left-[50%] after:rounded-[10px] after:h-[2px] after:bg-white hover:after:w-full after:duration-300"
+                    : ""}`}
               >
-              Home
+                Home
               </NavLink>
-        </li>
-        <li>  <a
-              href="#work"
+            </li>
+            <li>
+              {" "}
+              <a
+                rel="noopener noreferrer"
+                href="#work"
                 onClick={() => setnavShow(!navShow)}
-                className={`leading-[26.59px] text-black text-base font-normal navlinkhover md:text-white flex items-center gap-[6px] ${({ isActive, isPending }) =>
-                    isActive ? "after:absolute relative hover:after:left-[0] after:bottom-0 after:w-0 after:left-[50%] after:rounded-[10px] after:h-[2px] after:bg-white hover:after:w-full after:duration-300" : ""
-                      }`}
+                className={`leading-[26.59px] text-black text-base font-normal navlinkhover md:text-white flex items-center gap-[6px] ${({
+                  isActive,
+                  isPending,
+                }) =>
+                  isActive
+                    ? "after:absolute relative hover:after:left-[0] after:bottom-0 after:w-0 after:left-[50%] after:rounded-[10px] after:h-[2px] after:bg-white hover:after:w-full after:duration-300"
+                    : ""}`}
               >
-              How It Works
+                How It Works
               </a>
-              </li>
-              <li>  <NavLink
+            </li>
+            <li>
+              {" "}
+              <NavLink
                 onClick={() => setnavShow(!navShow)}
-                className={`leading-[26.59px] text-black text-base font-normal navlinkhover md:text-white flex items-center gap-[6px] ${({ isActive, isPending }) =>
-                    isActive ? "after:absolute relative hover:after:left-[0] after:bottom-0 after:w-0 after:left-[50%] after:rounded-[10px] after:h-[2px] after:bg-white hover:after:w-full after:duration-300" : ""
-                      }`}
+                className={`leading-[26.59px] text-black text-base font-normal navlinkhover md:text-white flex items-center gap-[6px] ${({
+                  isActive,
+                  isPending,
+                }) =>
+                  isActive
+                    ? "after:absolute relative hover:after:left-[0] after:bottom-0 after:w-0 after:left-[50%] after:rounded-[10px] after:h-[2px] after:bg-white hover:after:w-full after:duration-300"
+                    : ""}`}
               >
-          Features
+                Features
               </NavLink>
-              </li>
+            </li>
           </ul>
           <div className="max-md:px-[30px] max-md:flex justify-center max-md:w-full">
             <Buttons
@@ -60,7 +81,14 @@ const Nav = () => {
             />
           </div>
         </div>
-       <button onClick={() => setnavShow(!navShow ? true : false)} className={`menu-icon ${navShow ? "bg-transparent after:bg-black before:bg-black before:h-[1px] before:w-[14px] after:h-[1px] after:w-[14px] after:rotate-45 before:-rotate-45 " : "after:w-3 after:bg-white after:h-[3px] after:-top-[6px] after:left-0 after:rounded-full before:w-3 before:bg-white before:h-[3px] before:-bottom-[6px] before:right-0 before:rounded-full"} my-2.5`}></button>
+        <button
+          onClick={() => setnavShow(!navShow ? true : false)}
+          className={`menu-icon ${
+            navShow
+              ? "bg-transparent after:bg-black before:bg-black before:h-[1px] before:w-[14px] after:h-[1px] after:w-[14px] after:rotate-45 before:-rotate-45 "
+              : "after:w-3 after:bg-white after:h-[3px] after:-top-[6px] after:left-0 after:rounded-full before:w-3 before:bg-white before:h-[3px] before:-bottom-[6px] before:right-0 before:rounded-full"
+          } my-2.5`}
+        ></button>
       </div>
     </nav>
   );
