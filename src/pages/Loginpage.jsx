214be78934338200b8d95975ad02a85f9ccda500
Shreya-454 <bshreya932@GMAIL.COM>
2024-06-06 11:44:27 +0530

    footer

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -3,7 +3,7 @@ import Header from '../components/loginpage/Header'
 import Universal from '../components/loginpage/Universal'
 import AnyOne from '../components/loginpage/AnyOne'
 import Work from '../components/loginpage/Work'
-import Footer from '../components/homepage/Footer'
+import Footer from '../components/common/Footer'
 
 const Loginpage = () => {
   return (
